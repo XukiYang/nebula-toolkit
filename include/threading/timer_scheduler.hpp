040d15f3dde2b7cf50d5999d3741874f8e0d3ae6
yoki <1841266587@qq.com>
2025-05-15 22:55:31 +0800

    feat：基本完成定时器逻辑
    - 基于优先级队列
    - 基于线程池，自带调度器管理定时任务

--- include/threading/timer_scheduler.hpp
+++ include/threading/timer_scheduler.hpp
@@ -13,20 +13,27 @@ struct TimerTask {
 
 class TimerScheduler {
 private:
-  std::priority_queue<TimerTask> tasks_; // 优先级队列(小根堆)
-  std::unordered_set<uint64_t> canceled_ids_;
-  std::unique_ptr<std::thread *> scheduler_thread_; // 调度器线程
-  std::unique_ptr<ThreadPool> tp_;                  // 线程池
-  std::atomic<uint64_t> next_id_;                   // 任务ID
-  std::mutex mutex_;                                // 保护优先级队列锁
-  std::atomic<bool> running_{false}; // 调度器线程执行原子
+  std::priority_queue<TimerTask> timer_tasks_;    // 优先级队列(小根堆)
+  std::unordered_set<uint64_t> canceled_ids_;     // 退出任务ID set
+  std::unique_ptr<std::thread> scheduler_thread_; // 调度器线程
+  std::condition_variable scheduler_cv_;          // 调度器线程条件变量
+  std::unique_ptr<ThreadPool> thread_pool_;       // 线程池
+  std::atomic<uint64_t> next_id_{0};              // 任务ID
+  std::mutex mutex_;                              // 保护优先级队列锁
+  std::atomic<bool> running_{false};              // 调度器线程执行原子
 
 public:
-  TimerScheduler(size_t thread_count = std::thread::hardware_concurrency())
-      : tp_(std::make_unique<ThreadPool>(thread_count)){
+  explicit TimerScheduler(
+      size_t thread_count = std::thread::hardware_concurrency())
+      : thread_pool_(std::make_unique<ThreadPool>(thread_count)){
 
         };
-  ~TimerScheduler(){};
+  ~TimerScheduler() {
+    Stop();
+    if (scheduler_thread_ && scheduler_thread_->joinable()) {
+      scheduler_thread_->join();
+    }
+  };
 
 public:
   /// @brief 提交定时任务
@@ -38,9 +45,10 @@ public:
     auto now = std::chrono::steady_clock::now();
     {
       std::lock_guard<std::mutex> lock(mutex_);
-      tasks_.push(
+      timer_tasks_.push(
           {now + std::chrono::milliseconds(delay_ms), std::move(cb_task), id});
     }
+    scheduler_cv_.notify_one();
     return id;
   };
 
@@ -50,24 +58,61 @@ public:
   bool Cancel(uint64_t task_id) {
     std::lock_guard<std::mutex> lock(mutex_);
     canceled_ids_.insert(task_id);
+    return true;
   }
 
   /// @brief 启动调度器
   void Start() {
-    if (!running_) {
-      running_ = true;
+    if (!running_.load()) {
+      running_.store(true);
       scheduler_thread_ =
-          std::make_unique<std::thread *>(&TimerScheduler::RunScheduler, this);
+          std::make_unique<std::thread>(&TimerScheduler::RunScheduler, this);
     }
   };
 
   /// @brief 停止调调度器
-  void Stop(){
-
+  void Stop() {
+    if (running_) {
+      {
+        std::lock_guard<std::mutex> lock(mutex_);
+        running_ = false;
+      }
+      // 唤醒线程退出
+      scheduler_cv_.notify_all();
+      if (scheduler_thread_->joinable()) {
+        scheduler_thread_->join();
+      }
+    }
   };
 
   /// @brief 调度器主循环
-  void RunScheduler(){
+  void RunScheduler() {
+    while (running_.load()) {
+      std::unique_lock<std::mutex> lock(mutex_);
+      // 若无定时任务，则主循环线程休息
+      if (timer_tasks_.empty()) {
+        scheduler_cv_.wait(
+            lock, [this] { return !running_ || !timer_tasks_.empty(); });
+        continue;
+      }
+      // 判断是否为取消任务ID
+      auto task_id = timer_tasks_.top().task_id;
+      if (canceled_ids_.count(task_id)) {
+        timer_tasks_.pop();           // 出队
+        canceled_ids_.erase(task_id); // 删除
+        continue;
+      }
 
+      const auto &next_timer_task = timer_tasks_.top();
+      // 到时间的任务则提交到线程池托管执行
+      if (std::chrono::steady_clock::now() >= next_timer_task.exec_time) {
+        auto cur_timer_task = next_timer_task;
+        timer_tasks_.pop(); // 出队
+        lock.unlock();      // 释放锁后再提交任务
+        thread_pool_->PostTask(std::move(cur_timer_task.callback));
+      } else {
+        scheduler_cv_.wait_until(lock, next_timer_task.exec_time);
+      }
+    }
   };
 };
---
+++ tests/unit/timer_scheduler.cpp
@@ -0,0 +1,30 @@
+#include "../../include/threading/timer_scheduler.hpp"
+#include "../../include/logkit/logkit.hpp"
+#include <unistd.h>
+
+int main() {
+  TimerScheduler timer(2); // 2个线程
+  timer.Start();
+  LOG_MSG("Start");
+  // 基本测试
+  timer.ScheduleOnce(1000, []() {
+    LOG_MSG("1秒后执行");
+    return 0;
+  });
+  timer.ScheduleOnce(2000, []() {
+    LOG_MSG("2秒后执行");
+    return 0;
+  });
+
+  // 取消测试
+  auto cancel_id = timer.ScheduleOnce(3000, []() {
+    LOG_MSG("这个应该看不到");
+    return 0;
+  });
+  timer.Cancel(cancel_id);
+
+  // 保持运行
+  sleep(10);
+  timer.Stop();
+  LOG_MSG("测试结束");
+}
