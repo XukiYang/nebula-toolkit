aa50d7c1bec71ab2fe6647fd359db810f423c079
yanghouqi <yhq11050915@outlook.com>
2025-05-23 17:31:53 +0800

    feat：基于实用性优化回调接口

--- include/containers/unpacker.hpp
+++ include/containers/unpacker.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 #include "../logkit/logkit.hpp"
 #include "ring_buffer.hpp"
@@ -6,14 +5,21 @@
 
 namespace containers {
 
-// 头长度回调
-using HeadSzCb = std::function<size_t()>;
-// 解析根据值大小判定剩余数据大小的回调
-using DataSzCb = std::function<size_t(const uint8_t *data)>;
-// 尾长度回调
-using TailSzCb = std::function<size_t()>;
-// 用于校验的回调
-using CheckSzCb = std::function<size_t()>;
+/** 解析字节数回调:外部定义解析逻辑
+ * *head_ptr:解析数据头指针
+ * &head_size:获取头大小
+ * &data_size:获取头尾之外数据大小
+ * &tail_size:获取尾数据大小
+ */
+using DataSzCb = std::function<void(const uint8_t *head_ptr, size_t &head_size,
+                                    size_t &data_size, size_t &tail_size)>;
+/** 校验检查回调:外部传入特定校验逻辑
+ * *data_ptr:校验数据指针
+ * &data_len:获取校验数据长度
+ * bool ret:返回是否有效或无效
+ */
+using CheckValidCb = std::function<bool(const uint8_t *data_ptr)>;
+
 // 头定位符
 using HeadKey = std::vector<uint8_t>;
 // 尾定位符
@@ -27,7 +33,27 @@ public:
   UnPacker(HeadKey &&head_key = {}, TailKey &&tail_key = {},
            size_t buffer_size = 1024)
       : RingBuffer(buffer_size), head_key_(std::move(head_key)),
-        tail_key_(std::move(tail_key)) {}
+        tail_key_(std::move(tail_key)) {
+    unpacker_model_ = CheckModel();
+  }
+
+  UnPacker(HeadKey &&head_key = {}, TailKey &&tail_key = {},
+           DataSzCb data_sz_cb, CheckValidCb check_sz_cb,
+           size_t buffer_size = 1024)
+      : RingBuffer(buffer_size), head_key_(std::move(head_key)),
+        data_sz_cb_(data_sz_cb), check_sz_cb_(check_sz_cb),
+        tail_key_(std::move(tail_key)) {
+    unpacker_model_ = CheckModel();
+  }
+
+  UnpackerModel CheckModel() {
+    if (!head_key_.empty() && tail_key_.empty())
+      return UnpackerModel::kHead;
+    else if (!head_key_.empty() && !tail_key_.empty())
+      return UnpackerModel::kHeadTail;
+    else if (data_sz_cb_ && check_sz_cb_)
+      return UnpackerModel::KHeadTailCb;
+  };
 
   /// @brief 提交数据并解析数据包
   /// @param write_data
@@ -54,16 +80,20 @@ private:
   UnPackerResult GetPack(std::vector<std::vector<uint8_t>> &read_data) {
 
     // 仅头定位符（包含头定位符）
-    if (!head_key_.empty() && tail_key_.empty()) {
+    if (unpacker_model_ == UnpackerModel::kHead) {
       return ProcessHeadOnlyMode(read_data);
     }
     // 头和尾定位符（包含头尾定位符）
-    else if (!head_key_.empty() && !tail_key_.empty()) {
+    else if (unpacker_model_ == UnpackerModel::kHeadTail) {
       return ProcessHeadTailMode(read_data);
+    }
+    // 头尾加字节数与校验回调模式
+    else if (unpacker_model_ == UnpackerModel::KHeadTailCb) {
+      return ProcessHeadTailAndCbMode(read_data);
     }
     // 其他
     else {
-      LOG_DEBUG("头尾标记为空");
+      LOG_DEBUG("未知模式");
       return UnPackerResult::kError;
     }
   }
@@ -228,10 +258,68 @@ private:
     return UnPackerResult::kSuccess;
   };
 
+  UnPackerResult
+  ProcessHeadTailAndCbMode(std::vector<std::vector<uint8_t>> &read_data) {
+    // 查找头定位符
+    size_t head_pos = FindKey(head_key_, history_find_key_pos_);
+    LOGP_DEBUG("head_pos:%d,history_find_key_pos:%d", head_pos,
+               history_find_key_pos_);
+    if (head_pos == buffer_.size()) {
+      return UnPackerResult::kError;
+    }
+    while (AvailableToRead()) {
+      size_t head_size = 0, data_size = 0, tail_size = 0;
+      data_sz_cb_(buffer_.data() + head_pos, head_size, data_size, tail_size);
+      size_t data_len = head_size + data_size + tail_size;
+
+      // 若解出来的完整包长小于头尾定位符，则跳出此包
+      if (data_len < head_key_.size() + tail_key_.size()) {
+        continue;
+      }
+      // 基于头位置，查找包尾，判定是否符合预期
+      size_t tail_pos = FindKey(tail_key_, head_pos + head_key_.size());
+      if (tail_pos - head_pos != data_len) {
+        continue;
+      }
+      history_find_key_pos_ = tail_pos + tail_key_.size();
+      std::vector<uint8_t> packet(data_len);
+
+      // 计算线性可读空间
+      size_t first_chunk = std::min(data_len, buffer_.size() - tail_pos);
+      memcpy(packet.data(), buffer_.data() + head_pos, first_chunk);
+
+      // 如果包数据超过了线性可读空间，则发生环回
+      if (data_len > first_chunk) {
+        // 拷贝环回后的剩余数据
+        memcpy(packet.data() + first_chunk, buffer_.data(),
+               data_len - first_chunk);
+      }
+      bool check_ret =
+          CheckValidCb(static_cast<const uint8_t *>(packet.data()));
+
+      read_data.emplace_back(std::move(packet));
+      CommitReadSize(data_len);
+      LOGP_DEBUG("head_pos:%d,tail_pos:%d,data_len:%d,first_chunk:%d", head_pos,
+                 tail_pos, data_len, first_chunk);
+
+      // 基于尾定位符位置，查找下一包的头定位符位置
+      size_t head_pos_t = FindKey(head_key_, tail_pos + tail_key_.size());
+      if (head_pos_t == buffer_.size()) {
+        LOGP_DEBUG("not found next pack,pos:%d", head_pos_t);
+        return UnPackerResult::kError;
+      }
+      head_pos = head_pos_t;
+    }
+  }
+
 private:
   HeadKey head_key_;
   TailKey tail_key_;
   size_t history_find_key_pos_ = 0;
+
+  DataSzCb data_sz_cb_;
+  CheckValidCb check_sz_cb_;
+  UnpackerModel unpacker_model_;
 };
 
 } // namespace containers
